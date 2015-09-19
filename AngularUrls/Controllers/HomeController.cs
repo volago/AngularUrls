62db1c150e498e08139852064c85ed94cc622508
Walter Luszczyk <walter.luszczyk@outlook.com>
2015-09-19 23:59:18 +0200

    redirect bota w html5 mode

--- AngularUrls/Controllers/HomeController.cs
+++ AngularUrls/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,12 +11,40 @@ namespace AngularUrls.Controllers
     {
         public ActionResult Index()
         {
-            var or = Request.ServerVariables["HTTP_X_ORIGINAL_URL"];
 
-            var xx = Request.ServerVariables["SCRIPT_NAME"];
-            var yy = Request.ServerVariables["QUERY_STRING"];
+            string fragment = Request.QueryString["_escaped_fragment_"];
 
-            return View();
+            if (fragment != null)
+            {
+                try
+                {
+                    var file = Server.MapPath(string.Format(@"~/App/redirected.html"));
+                    if (!string.IsNullOrWhiteSpace(file))
+                    {
+                        var fileContent = System.IO.File.ReadAllText(file);
+                        return Content(fileContent);
+                    }
+
+                    var message = string.Format("Offline content not found. Escaped fragment: '{0}'", fragment);
+                    return Content(message);
+                }
+                catch (Exception ex)
+                {
+                    var sb = new StringBuilder();
+                    sb.AppendLine(ex.Message);
+                    if (ex.InnerException != null)
+                    {
+                        sb.AppendLine("------------------------------");
+                        sb.AppendLine(ex.InnerException.Message);
+                    }
+                    return Content(ex.Message);
+                }
+            }
+
+
+            ErrorViewModel vm = new ErrorViewModel();
+
+            return View(vm);
         }
     }
 }
